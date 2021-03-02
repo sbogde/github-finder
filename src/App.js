435917dc52345150f932d10e42d13b27dbbdfa4c
Sorin <Sorin.Bogde@Chubb.com>
2021-03-02 14:58:37 +0000

    the Alert component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import axios from 'axios';
@@ -11,6 +11,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   static propType = {
@@ -67,16 +68,32 @@ class App extends Component {
     });
   };
 
+  setAlert = (msg, type) => {
+    this.setState({
+      alert: {
+        msg,
+        type,
+      },
+    });
+    setTimeout(() => {
+      this.setState({
+        alert: null,
+      });
+    }, 5000);
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
         <div className='container'>
+          <Alert alert={alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
