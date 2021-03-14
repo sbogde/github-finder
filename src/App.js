d0e8d88adcc4b4d26f268ce55408537d717f2e94
Sorin <Sorin.Bogde@Chubb.com>
2021-03-14 09:34:39 +0000

    Creating Reducers

--- src/App.js
+++ src/App.js
@@ -18,17 +18,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  const searchUsers = async (text) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET_ID}`
-    );
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   const getUser = async (username) => {
     setLoading(true);
 
@@ -76,7 +65,6 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0}
                       setAlert={showAlert}
